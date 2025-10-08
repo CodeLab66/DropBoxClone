15d93c316042ad8b0e56d463f528b3cfde2b80dd
Muhammad-Okasha-Khan <149578718+Muhammad-Okasha-Khan@users.noreply.github.com>
2025-10-08 06:10:15 +0500

    made "config.c"
    Removed unnecessary const variable declarations that are defined as macros in config.h.

---
+++ config.c
@@ -0,0 +1,12 @@
+#include "config.h"
+
+// No need for const variable declarations here since they are defined as macros in config.h
+// Remove these lines:
+// const int PORT = 8080;
+// const int MAX_QUEUE_SIZE = 100;
+// const int NUM_CLIENT_THREADS = 4;
+// const int NUM_WORKER_THREADS = 4;
+// const int BUFFER_SIZE = 1024;
+// const int MAX_USERS = 100;
+// const long DEFAULT_QUOTA = 10485760L; // 10 MB
+// const char *ROOT_DIR = "./storage";
